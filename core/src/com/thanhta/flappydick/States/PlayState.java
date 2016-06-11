d280a7ac46a3f150e5d5ce9cf8d6399595456c57
tathanh03 <ta.thanh.03@gmail.com>
2016-06-12 00:06:47 +0700

    Add tubes, bird now can fly

--- core/src/com/thanhta/flappydick/States/PlayState.java
+++ core/src/com/thanhta/flappydick/States/PlayState.java
@@ -1,27 +1,52 @@
 package com.thanhta.flappydick.States;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 import com.thanhta.flappydick.FlappyDick;
+import com.thanhta.flappydick.sprites.Bird;
+import com.thanhta.flappydick.sprites.Tube;
 
 public class PlayState extends State {
-    private Texture bird;
+    private static final int TUBE_SPACING = 125;
+    private static final int TUBE_COUNT = 4;
+    private Bird bird;
+    private Texture bg;
+    private Array<Tube> tubes;
+
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
-        //set the game world's view smaller
-        //"false" to count form bottom left
+        bird = new Bird(50,300);
+        //set the game world's view smaller, "false" to count form bottom left
         cam.setToOrtho(false, FlappyDick.WIDTH/2, FlappyDick.HEIGHT/2);
+        bg = new Texture("bg.png");
+        tubes = new Array<Tube>();
+
+        for (int i=1; i<= TUBE_COUNT; i++){
+            tubes.add(new Tube(i*(TUBE_SPACING + Tube.TUBE_WIDTH)));
+
+        }
     }
 
     @Override
     public void handleInput() {
-
+        if (Gdx.input.justTouched()){bird.jump();}
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
+        bird.update(dt);
+        //adjust cam to follow the bird position
+        cam.position.x = bird.getPosition().x +80;
+        //remove went off screen
+        for (Tube tube: tubes){
+            if (cam.position.x - (cam.viewportWidth/2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()){
+                tube.reposition(tube.getPosTopTube().x+((Tube.TUBE_WIDTH + TUBE_SPACING)* TUBE_COUNT));
+            }
+        }
+        cam.update();
     }
 
     @Override
@@ -29,7 +54,13 @@ public class PlayState extends State {
         //adjust to the right game world's viewport
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bg, cam.position.x - (cam.viewportWidth/2), 0);
+        sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        for (Tube tube: tubes) {
+            sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+            sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
+        }
+
         sb.end();
     }
 
---
+++ core/src/com/thanhta/flappydick/sprites/Tube.java
@@ -0,0 +1,43 @@
+package com.thanhta.flappydick.sprites;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+public class Tube {
+
+    private Texture topTube, bottomTube;
+    private Vector2 posTopTube, posBotTube;
+    private Random rand;
+    public static final int TUBE_WIDTH = 52;
+    private static final int FLUCTUATION = 130;
+    private static final int TUBE_GAP = 100;
+    private static final int LOWEST_OPENING = 120;
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+    public Vector2 getPosBotTube() {
+        return posBotTube;
+    }
+
+    public Tube (float x){
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+        rand = new Random();
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+    }
+
+    public void reposition(float x){
+        posTopTube.set(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+
+    }
+}
