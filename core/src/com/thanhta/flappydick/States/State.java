788fceeea8b21e16a2df24d100a51f60e9e9b9cd
tathanh03 <ta.thanh.03@gmail.com>
2016-06-11 17:25:31 +0700

    Create State in stack form

--- core/src/com/thanhta/flappydick/States/State.java
+++ core/src/com/thanhta/flappydick/States/State.java
@@ -6,16 +6,17 @@ import com.badlogic.gdx.math.Vector3;
 
 public abstract class State {
     //each state need a camera to locate the position in the world
-//    protected OrthographicCamera cam;
-//    protected Vector3 mouse;
-//    protected GameStateManager gsm;
-//    protected State(GameStateManager gsm){
-//        this.gsm = gsm;
-//        cam = new OrthographicCamera();
-//        mouse = new Vector3();
-//    }
-//    protected abstract void handleInput();
-//    public abstract void update(float dt);
-//    public abstract void render(SpriteBatch sb);
+    protected OrthographicCamera cam;
+    protected Vector3 mouse;
+    protected GameStateManager gsm;
+
+    public State(GameStateManager gsm){
+        this.gsm = gsm;
+        cam = new OrthographicCamera();
+        mouse = new Vector3();
+    }
+    public abstract void handleInput();
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch sb);
 
 }
